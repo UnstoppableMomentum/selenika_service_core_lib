3dc51b5989675cbe14d6c59eefc766555a3f8ef3
Serg <sobolentsev@gmail.com>
2022-10-10 19:34:32 +0300

    Add test 'peer server'

--- src/command_dispatcher/command_dispatcher_base.cpp
+++ src/command_dispatcher/command_dispatcher_base.cpp
@@ -6,18 +6,18 @@
 
 #include "command_dispatcher_base.h"
 
+#include "command_dispatcher_base_commands.h"
 #include "command_parser_join.h"
 #include "command_parser_send_message.h"
 #include "command_handler_join.h"
 #include "command_handler_send_message.h"
 
 CommandDispatcherBase::CommandDispatcherBase() {
-    selenika::core::service::Dispatcher<MessageProcessingData> commandDispatcher2_;
-    AddHandler<ParserCmdJoin>(1, processRequestSignIn);
-    AddHandler<CmdParserSendMessage>(3, sendMessage);
+    AddHandler<ParserCmdJoin>(selenika::core::service::kCmdConnect, processRequestSignIn);
+    // TODO implement disconnect
 }
 
-CommandDispatcherBase::~CommandDispatcherBase() {}
+CommandDispatcherBase::~CommandDispatcherBase() = default;
 
 std::string CommandDispatcherBase::DispatchCommand(MessageProcessingData &&messageProcessingData) {
     selenika::core::service::CommandResult res = commandDispatcher_.DispatchCommand(messageProcessingData.getCommand(), messageProcessingData);
